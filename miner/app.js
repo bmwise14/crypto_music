963d8630c9cd25e0337951ca79d9c30ed5bac86c
Rongxin Zhang <mrrongxin@gmail.com>
2018-11-13 13:32:43 -0500

    Update reader

--- miner/app.js
+++ miner/app.js
@@ -2,12 +2,53 @@ const express = require('express')
 const request = require('superagent');
 const fs = require('fs');
 
+var SerialPort = require('serialport'); // serial library
+
 const app = express()
 const port = 3000
 
+var Readline = SerialPort.parsers.Readline; // read serial data as lines
+
+var serial_port = '/dev/ttyUSB0'
+
+const serial = new SerialPort(serial_port, {baudRate: 9600});
+
+
 app.get('/', (req, res) => res.send('We are mining '))
 
-setInterval(()=>{
+// setInterval(()=>{ sendToServer }, 1000)
+
+setInterval(()=>{ writeToSerial }, 1000)
+
+readFromSerial()
+
+function writeToSerial(){
+  
+  serial.write('main screen turn on\n', function(err) {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('message written');
+  });
+}
+
+function readFromSerial(){
+  
+  const parser = new Readline({
+    delimiter: '\r\n'
+  });
+  
+  // Read data that is available on the serial port and send it to the websocket
+  serial.pipe(parser);
+  parser.on('data', function(data) {
+    console.log('Data:', data);
+  });
+}
+
+/*
+  Send stuff to server
+ */
+function sendToServer(){
   // var hash = fs.readFileSync('../hashes.txt', 'utf8');
   // var result = fs.readFileSync('../result.txt', 'utf8');
 
@@ -33,7 +74,6 @@ setInterval(()=>{
   // var success = result.split(',')[0]
   // var hashes_computed = result.split(',')[1]
   // console.log("--> hash", success, hashes_computed);
-
-}, 1000)
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
